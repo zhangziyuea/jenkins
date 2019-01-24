c7f197240eaaa379c7bbec1eaa4877b6e1ade565
zhangziyue <927231981@qq.com>
2019-01-24 16:33:11 +0800

    rhrthrth

---
+++ scripts/test_login.py
@@ -0,0 +1,15 @@
+import allure
+def test_a():
+    print("aaaa")
+    assert 1
+
+
+@allure.step(title="测试注册的过程")
+def test_login():
+    allure.attach("注册","输入用户名")
+    print("aaaa")
+    allure.attach("注册","输入密码")
+    print("bbbb")
+    allure.attach("注册","点击注册")
+    print("cccc")
+    assert 0
